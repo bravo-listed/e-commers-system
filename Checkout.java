d45f5953253d160759c914f9a3e8410d52de5bc7
Jovanovic Aleksandar <153069356+bravo-listed@users.noreply.github.com>
2023-12-07 13:07:44 +0700

    Create Checkout.java

---
+++ Checkout.java
@@ -0,0 +1,8 @@
+// Checkout.java
+public class Checkout {
+    public void processCheckout(ShoppingCart cart) {
+        // Implement checkout logic here
+        // Validate data, update the database, etc.
+        System.out.println("Checkout processed successfully!");
+    }
+}
